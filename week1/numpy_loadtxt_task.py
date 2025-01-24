a31847bb2689cf32ffcfa5bee693198ddf63e4df
Srujan Dandu <srujandandu01@gmail.com>
2025-01-23 20:52:23 -0700

    edited code to be a single function which only works if __name__ == __main__

--- week1/numpy_loadtxt_task.py
+++ week1/numpy_loadtxt_task.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 
 """This code takes two columns of values from a text file and plots them against each other."""
 
-# extracting data from text file
-data = np.loadtxt('/d/users/srujan/ASTR5160/week1/data.txt')
+def main():
 
-# extracting the two columns from the data array
-x = data[:,0]
-y = data[:,1]
+	# extracting data from text file
+	data = np.loadtxt('/d/users/srujan/ASTR5160/week1/data.txt')
 
-# plotting the two columns against each other
-plt.plot(x, y)   # plotting a straight line
-plt.scatter(x, y, c='yellow', marker='+')   # plotting as yellow crosses
-plt.show()
+	# extracting the two columns from the data array
+	x = data[:,0]
+	y = data[:,1]
+
+	# plotting the two columns against each other
+	plt.plot(x, y)   # plotting a straight line
+	plt.scatter(x, y, c='yellow', marker='+')   # plotting as yellow crosses
+	plt.show()
+
+if __name__ == "__main__":
+	main()
