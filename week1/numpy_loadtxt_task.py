e7863bbc974e80d2fba01d4d775aabcc61623603
Srujan Dandu <srujandandu01@gmail.com>
2025-01-23 20:48:29 -0700

    wrote code to read two columns of values in text file and plot them against each other

---
+++ week1/numpy_loadtxt_task.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""This code takes two columns of values from a text file and plots them against each other."""
+
+# extracting data from text file
+data = np.loadtxt('/d/users/srujan/ASTR5160/week1/data.txt')
+
+# extracting the two columns from the data array
+x = data[:,0]
+y = data[:,1]
+
+# plotting the two columns against each other
+plt.plot(x, y)   # plotting a straight line
+plt.scatter(x, y, c='yellow', marker='+')   # plotting as yellow crosses
+plt.show()
