b867991237ffe0ca233833b3721a46c5dba955e8
Srujan Dandu <srujandandu01@gmail.com>
2025-01-27 18:09:54 -0700

    plotted dec vs ra from fits file, and added comments

--- week2/recarrays.py
+++ week2/recarrays.py
@@ -1,3 +1,11 @@
 from astropy.table import Table
+import matplotlib.pyplot as plt
 
+# SD calling in fits file
 objs = Table.read("/d/scratch/ASTR5160/week2/struc.fits")
+
+# SD plotting dec vs ra
+plt.scatter(objs['RA'], objs['DEC'], s=10, edgecolor='black')
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$\delta$')
+plt.show()
