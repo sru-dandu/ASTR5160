e8e686efb16995c8dafd395563ad9868e1fd4704
Srujan Dandu <srujandandu01@gmail.com>
2025-01-28 15:36:14 -0700

    Used Boolean indexing to plot RA and DEC of data points with extinction > 0.22

--- week2/recarrays.py
+++ week2/recarrays.py
@@ -10,5 +10,18 @@ plt.xlabel(r'$\alpha$')
 plt.ylabel(r'$\delta$')
 plt.show()
 
-# SD printing first column of 'extinction' 5-array
+
+
+# SD printing first column of 'extinction' 5-array (100 rows, 5 columns)
 print(objs['EXTINCTION'][:,0])
+
+
+
+# SD creating Boolean mask for extinction values > 0.22
+ext_mask = objs['EXTINCTION'][:,0] > 0.22
+
+# SD plotting the plot from above, but using the new Boolean mask
+plt.scatter(objs['RA'][ext_mask], objs['DEC'][ext_mask], s=10, edgecolor='black')
+plt.xlabel(r'$\alpha$')
+plt.ylabel(r'$\delta$')
+plt.show()
