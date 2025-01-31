1b74d84a3e250a3910e91809ab22b396b1e11d58
Srujan Dandu <srujandandu01@gmail.com>
2025-01-30 17:27:04 -0700

    found an object's airmass if observed at 11pm tonight and 11pm 1 month from now

--- week2/survey_observations.py
+++ week2/survey_observations.py
@@ -1,4 +1,4 @@
-from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astropy.time import Time
 import numpy as np
 import astropy.units as u
@@ -70,3 +70,30 @@ WIRO_loc = EarthLocation(lat=WIRO_lat, lon=WIRO_lon, height=WIRO_alt)
 # SD printing EarthLocation for WIRO
 print("---TASK 5---")
 print(f"The geodetic location of WIRO is {WIRO_loc}")   # SD geodetic is for input of lon, lat, height (from EarthLocation documentation)
+
+
+
+### TASK 6 (BLACK) ###
+
+# SD saving coords of object
+obj_ra = 12 * u.h
+obj_dec = 30 * u.deg
+obj_coords = SkyCoord(ra=obj_ra, dec=obj_dec)
+
+# SD finding time of observation
+utcoffset = -7 * u.h   # SD MST is 7 hours behind UTC
+time_today = Time("2025-1-30 23:00:00") - utcoffset   # SD if observing tonight at 11 PM
+time_1month = Time("2025-3-1 23:00:00") - utcoffset   # SD if observing 1 month (30 days) from today, at 11 PM
+
+# SD find Alt,Az coordinates of object as observed from WIRO at the mentioned times
+obj_altaz_today = obj_coords.transform_to(AltAz(obstime=time_today, location=WIRO_loc))
+obj_altaz_1month = obj_coords.transform_to(AltAz(obstime=time_1month, location=WIRO_loc))
+
+# SD convert alt,az to airmass
+obj_airmass_today = obj_altaz_today.secz
+obj_airmass_1month = obj_altaz_1month.secz
+
+# SD printing the airmasses
+print("---TASK 6---")
+print(f"The airmass of the object tonight at 11 PM is {obj_airmass_today}")
+print(f"The airmass of the object 1 month from now at 11 PM is {obj_airmass_1month}")
