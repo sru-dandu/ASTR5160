0069379fdf29cfd5b83029f72a9447a2e386c30d
Srujan Dandu <srujandandu01@gmail.com>
2025-02-03 16:51:53 -0700

    checked that conversion to cartesian using astropy matches manual conversion

--- weekly-tasks/week03/coordinate_transforms.py
+++ weekly-tasks/week03/coordinate_transforms.py
@@ -1,4 +1,5 @@
 from astropy.coordinates import SkyCoord
+import numpy as np
 
 
 
@@ -8,14 +9,29 @@ from astropy.coordinates import SkyCoord
 #SD coords obtained from Google
 fomalhaut_coords = SkyCoord('22h57m39s', '-29d37m20s')
 
-#SD saving RA and DEC (in degrees)
-fomalhaut_ra = fomalhaut_coords.ra.deg
-fomalhaut_dec = fomalhaut_coords.dec.deg
+#SD saving RA and DEC (in radians)
+#SD need radians in order to be used with np.cos and np.sin
+fomalhaut_ra = fomalhaut_coords.ra.rad
+fomalhaut_dec = fomalhaut_coords.dec.rad
 
 #SD convert RA and DEC to cartesian coords
 fomalhaut_coords.representation_type = "cartesian"
 
-#SD printing the resulting values
-print(fomalhaut_ra)
-print(fomalhaut_dec)
-print(fomalhaut_coords)
+#SD printing the RA and DEC values, in radians
+print("TASK 1")
+print("------")
+print(f"The right ascension of Fomalhaut is {fomalhaut_ra} radians")
+print(f"The declination of Fomalhaut is {fomalhaut_dec} radians")
+
+#SD check conversions using equations from slides
+#SD converting radians to cartesian
+fomalhaut_x = np.cos(fomalhaut_ra) * np.cos(fomalhaut_dec)
+fomalhaut_y = np.sin(fomalhaut_ra) * np.cos(fomalhaut_dec)
+fomalhaut_z = np.sin(fomalhaut_dec)
+
+#SD printing the cartesian coords obtained through both methods
+print("Converting the RA and DEC into cartesian coordinates gives:")
+print( "        |          x         |          y           |            z   ")
+print(f"astropy | {fomalhaut_coords.x} | {fomalhaut_coords.y} | {fomalhaut_coords.z}")
+print(f" manual | {fomalhaut_x} | {fomalhaut_y} | {fomalhaut_z}")
+
