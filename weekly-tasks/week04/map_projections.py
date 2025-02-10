e48105177645be309e0996d0116074ab838051ec
Srujan Dandu <srujandandu01@gmail.com>
2025-02-10 16:35:34 -0700

    plotted Aitoff projection of dec vs ra. Also converted to Cartesian coordinates and plotted Aitoff projection of y vs x

--- weekly-tasks/week04/map_projections.py
+++ weekly-tasks/week04/map_projections.py
@@ -1,6 +1,9 @@
 import numpy as np
 from numpy.random import random
 import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy import wcs
 
 
 
@@ -46,3 +49,48 @@ ax2.set_ylabel("dec [deg]", weight=800)
 
 plt.show()
 
+
+
+### TASK 3 (BLACK) ###
+
+#SD create a grid of ra and dec
+ra_task3 = np.arange(0.5, 359.5, 1)
+dec_task3 = np.arange(-89.5, 89.5, 1)
+ra_grid, dec_grid = np.meshgrid(ra_task3, dec_task3)
+
+#SD saving grid as SkyCoord obect
+grid = SkyCoord(ra_grid, dec_grid, unit=u.deg)
+
+#SD configure dustmaps
+from dustmaps.config import config
+dustdir = "/d/scratch/ASTR5160/data/dust/v0_1/maps"
+config["data_dir"] = dustdir
+from dustmaps.sfd import SFDQuery
+sfd = SFDQuery()
+
+#SD find reddening for grid
+grid_reddening = sfd(grid)
+
+#SD convert (ra, dec) to (x, y)
+w = wcs.WCS(naxis=2)
+w.wcs.ctype = ["RA---AIT", "DEC--AIT"]
+x_grid, y_grid = w.wcs_world2pix(ra_grid, dec_grid, 1)
+
+#SD plotting both coordinate systems
+fig = plt.figure(figsize=(16,20))
+
+#SD subplot with aitoff projection
+ax1 = fig.add_subplot(121, projection='aitoff')
+ax1.scatter(ra_grid, dec_grid, c='gray', alpha=0.5, s=2)
+ax1.grid(color='blue', linestyle='dashed', linewidth=2)
+ax1.set_xlabel("ra [deg]", weight=800)
+ax1.set_ylabel("dec [deg]", weight=800)
+
+#SD subplot with lambert projection
+ax2 = fig.add_subplot(122, projection='aitoff')
+ax2.scatter(ra, dec, c='gray', alpha=0.5, s=2)
+ax2.grid(color='blue', linestyle='dashed', linewidth=2)
+ax2.set_xlabel("x", weight=800)
+ax2.set_ylabel("y", weight=800)
+
+plt.show()
