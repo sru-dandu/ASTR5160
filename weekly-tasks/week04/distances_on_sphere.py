7b3f4723b314342a8af0e22f668f77fd9c1f8c75
Srujan Dandu <srujandandu01@gmail.com>
2025-02-11 13:56:06 -0700

    made two sets of 100 random (ra,dec) coordinates and plotted them

--- weekly-tasks/week04/distances_on_sphere.py
+++ weekly-tasks/week04/distances_on_sphere.py
@@ -1,6 +1,7 @@
 import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy import units as u
+import matplotlib.pyplot as plt
 
 
 
@@ -37,3 +38,23 @@ print("TASK 1:")
 print(f"The zenith angle (found manually) is {z_angle}")
 print("check:")
 print(f"The zenith angle (found using SkyCoord's 'separation' method) is {z_angle_check}")
+
+
+
+### TASK 2 (RED) ###
+
+#SD find 2 sets of 100 ra values between 2 hr and 3 hr
+ra_range1 = (np.random.random(100) + 2) * u.hour
+ra_range2 = (np.random.random(100) + 2) * u.hour
+
+#SD find 2 sets of 100 dec values between -2 deg and 2 deg
+dec_range1 = (4*np.random.random(100) - 2) * u.deg
+dec_range2 = (4*np.random.random(100) - 2) * u.deg
+
+#SD plotting dec vs ra for both sets
+plt.scatter(ra_range1, dec_range1, marker='o', c='steelblue', label='set 1')
+plt.scatter(ra_range2, dec_range2, marker='x', c='maroon', label='set 2')
+plt.xlabel("ra [hours]")
+plt.ylabel("dec [degrees]")
+plt.legend()
+plt.show()
