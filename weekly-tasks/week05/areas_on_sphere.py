fbbfe4cc952ce7ada3a35eaca30e40c3c1fe1681
Srujan Dandu <srujandandu01@gmail.com>
2025-02-20 14:59:50 -0700

    found the number of points in each pixel

--- weekly-tasks/week05/areas_on_sphere.py
+++ weekly-tasks/week05/areas_on_sphere.py
@@ -21,4 +21,26 @@ nside_area = hp.nside2pixarea(1, degrees=True)
 
 print("TASK 2:")
 print(f"The area of a HEALpixel when Nside = 1 is {nside_area} square degrees.")
+print('----------')
 
+
+
+### TASK 3 (RED) ###
+
+#SD find the number of points in each of the HEALpixels
+_, counts = np.unique(pix, return_counts=True)
+
+#SD find the fraction of points in each pixel, for easier comparision
+counts_frac = counts / np.sum(counts)
+
+print("TASK 3:")
+
+#SD print the percent of points for each pixel
+for i in range(len(counts_frac)):
+	print(f"In pixel {i+1}, there are {100*counts_frac[i]}% of the points.")
+print("The results are consistent with the fact that the pixels are equal-area.")
+print('----------')
+
+
+
+### TASK 4 (BLACK) ###
