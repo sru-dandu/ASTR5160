e84cac370ae916c7b08f9a9a3d1b0b9d200905da
Srujan Dandu <srujandandu01@gmail.com>
2025-02-24 15:37:23 -0700

    rewrote code from Task 1 as a defined function to make Task 2 easier

--- weekly-tasks/week06/masking.py
+++ weekly-tasks/week06/masking.py
@@ -13,42 +13,78 @@ from spherical_caps import cap_ra, cap_dec
 
 ### TASK 1 (RED) ###
 
-#SD save ra and dec bounds
-ra1 = 5*u.hr
-ra2 = 6*u.hr
-dec1 = 30*u.deg
-dec2 = 40*u.deg
+#SD defined function that takes two ra and two dec values and gives contents of Mangle file
+def Mangle_contents(ra1, ra2, dec1, dec2):
+	"""Takes 2 ra and 2 dec values, and writes the contents of a Mangle file.
+	File consists of 1 polygon made up of 4 caps, each one bounded by a given value.
+	
+	INPUTS
+	------
+	ra1 : class: 'astropy.units.quantity.Quantity'
+		The ra bound of the first spherical cap.
+		Must be in units of hours.
+	ra2 : class: 'astropy.units.quantity.Quantity'
+		The ra bound of the second spherical cap.
+		Must be in units of hours.
+	dec1 : class: 'astropy.units.quantity.Quantity'
+		The dec bound of the third spherical cap.
+		Must be in angular units.
+	dec2 : class: 'astropy.units.quantity.Quantity'
+		The dec bound of the fourth spherical cap.
+		Must be in angular units.
+	
+	RETURNS
+	-------
+	class: 'str'
+		The contents of a Mangle file.
+		Has one polygon made up of 4 caps.
+		Each cap is constrained by one of the given bounds.
+	class: 'numpy.float64'
+		The area (in str) of the lat-lon rectangle resulting from the 4 caps.
+	"""
+	
+	#SD find area of lat-lon rectangle, in units of str
+	ra1_rad = ra1 * (2*np.pi*u.rad) / (24*u.hr)
+	ra2_rad = ra2 * (2*np.pi*u.rad) / (24*u.hr)
+	area = (ra2_rad - ra1_rad) * (np.sin(dec2) - np.sin(dec1))
+	
+	#SD use week05 code to find 4-vectors of the caps
+	vector_ra1 = cap_ra(ra1)
+	vector_ra2 = cap_ra(ra2)
+	vector_dec1 = cap_dec(dec1)
+	vector_dec2 = cap_dec(dec2)
+	
+	#SD extract values from vectors
+	x1, y1, z1, one_minus_h1 = vector_ra1
+	x2, y2, z2, one_minus_h2 = vector_ra2
+	x3, y3, z3, one_minus_h3 = vector_dec1
+	x4, y4, z4, one_minus_h4 = vector_dec2
 
-#SD find area of lat-lon rectangle, in units of str
-ra1_rad = ra1 * (2*np.pi*u.rad) / (24*u.hr)
-ra2_rad = ra2 * (2*np.pi*u.rad) / (24*u.hr)
-area = (ra2_rad - ra1_rad) * (np.sin(dec2) - np.sin(dec1))
+	#SD write the string to be saved to intersection.ply
+	filestring = ["1 polygons\n",
+			f"polygon 1 ( 4 caps, 1 weight, 0 pixel, {area.value} str):\n",
+			f"\t{x1:19.16f} {y1:19.16f} {z1:19.16f} {one_minus_h1:19.16f}\n",
+			f"\t{x2:19.16f} {y2:19.16f} {z2:19.16f} {one_minus_h2:19.16f}\n",
+			f"\t{x3:19.16f} {y3:19.16f} {z3:19.16f} {one_minus_h3:19.16f}\n",
+			f"\t{x4:19.16f} {y4:19.16f} {z4:19.16f} {one_minus_h4:19.16f}"]
+	
+	return filestring, area.value
 
-#SD use week05 code to find 4-vectors of the caps
-vector_ra1 = cap_ra(ra1)
-vector_ra2 = cap_ra(ra2)
-vector_dec1 = cap_dec(dec1)
-vector_dec2 = cap_dec(dec2)
 
-#SD extract values from vectors
-x1, y1, z1, one_minus_h1 = vector_ra1
-x2, y2, z2, one_minus_h2 = vector_ra2
-x3, y3, z3, one_minus_h3 = vector_dec1
-x4, y4, z4, one_minus_h4 = vector_dec2
-
-#SD write the string to be saved to intersection.ply
-contents = ["1 polygons\n",
-		f"polygon 1 ( 4 caps, 1 weight, 0 pixel, {area.value} str):\n",
-		f"\t{x1:19.16f} {y1:19.16f} {z1:19.16f} {one_minus_h1:19.16f}\n",
-		f"\t{x2:19.16f} {y2:19.16f} {z2:19.16f} {one_minus_h2:19.16f}\n",
-		f"\t{x3:19.16f} {y3:19.16f} {z3:19.16f} {one_minus_h3:19.16f}\n",
-		f"\t{x4:19.16f} {y4:19.16f} {z4:19.16f} {one_minus_h4:19.16f}"]
+#SD run the defined function with given ra and dec bounds
+contents1, area1 = Mangle_contents(5*u.hr, 6*u.hr, 30*u.deg, 40*u.deg)
 
 #SD create files and write to them
 with open("masking-vectors.ply", "w") as f:
-	f.writelines(contents)
+	f.writelines(contents1)
 
 print('TASK 1:')
-print(f"The area of the lat-lon rectangle is {area.value} str.")
+print(f"The area of the lat-lon rectangle is {area1} str.")
 print("Vectors written to file.")
 print('----------')
+
+
+
+### TASK 2 (RED) ###
+
+
