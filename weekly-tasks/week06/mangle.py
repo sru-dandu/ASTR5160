a535a70290e908ba9bd44c000aa1b33a93723cc7
Srujan Dandu <srujandandu01@gmail.com>
2025-02-23 18:10:16 -0700

    saved cap vectors to two .ply files in Mangle format

--- weekly-tasks/week06/mangle.py
+++ weekly-tasks/week06/mangle.py
@@ -25,3 +25,36 @@ print('TASK 1:')
 print(f"cap1: {cap1_vector}")
 print(f"cap2: {cap2_vector}")
 print('----------')
+
+
+
+### TASK 2 (RED) ###
+
+#SD extract values from vectors
+x1, y1, z1, one_minus_h1 = cap1_vector
+x2, y2, z2, one_minus_h2 = cap2_vector
+
+#SD write the string to be saved to intersection.ply
+intersection_contents = ["1 polygons\n",
+		"polygon 1 ( 2 caps, 1 weight, 0 pixel, 0 str):\n",
+		f"\t{x1:19.16f} {y1:19.16f} {z1:19.16f} {one_minus_h1:19.16f}\n",
+		f"\t{x2:19.16f} {y2:19.16f} {z2:19.16f} {one_minus_h2:19.16f}"]
+		
+#SD write the string to be saved to bothcaps.ply
+bothcaps_contents =  ["2 polygons\n",
+		"polygon 1 ( 1 caps, 1 weight, 0 pixel, 0 str):\n",
+		f"\t{x1:19.16f} {y1:19.16f} {z1:19.16f} {one_minus_h1:19.16f}\n",
+		"polygon 2 ( 1 caps, 1 weight, 0 pixel, 0 str):\n",
+		f"\t{x2:19.16f} {y2:19.16f} {z2:19.16f} {one_minus_h2:19.16f}"]
+
+#SD create files and write to them
+with open("intersection.ply", "w") as f:
+	f.writelines(intersection_contents)
+with open("bothcaps.ply", "w") as f:
+	f.writelines(bothcaps_contents)
+
+print('TASK 2:')
+print("Written to files.")
+print('----------')
+
+
