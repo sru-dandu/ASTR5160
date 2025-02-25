91221124408eecdf3e099a2fea3a5ada10cdafe2
Srujan Dandu <srujandandu01@gmail.com>
2025-02-25 15:47:36 -0700

    wrote code to find airmasses for each object at a single date/time

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -1,6 +1,6 @@
-from astropy.coordinates import SkyCoord
-from astropy.coordinates import EarthLocation
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation
 from astropy.table import Table
+from astropy.time import Time
 from astropy import units as u
 import pandas
 import numpy as np
@@ -8,16 +8,35 @@ import numpy as np
 
 
 #SD read in file and extract ra and dec
-coords = pandas.read_csv('/d/scratch/ASTR5160/week4/HW1quasarfile.txt', header=None)[0]
+coords_data = pandas.read_csv('/d/scratch/ASTR5160/week4/HW1quasarfile.txt', header=None)[0]
 
 #SD add spaces in order to get in format hh mm ss +dd mm ss
 #SD this allows it to be read by SkyCoord
 coords_parsed = [(i[0:2] + ' ' + i[2:4] + ' ' + i[4:9] + ' '
-		+ i[9:12] + ' ' + i[12:14] + ' ' + i[14:]) for i in coords]
+		+ i[9:12] + ' ' + i[12:14] + ' ' + i[14:]) for i in coords_data]
 
 #SD pass coordinates through SkyCoord
-coords_radec = SkyCoord(coords_parsed, unit=(u.hr, u.deg))
+coords = SkyCoord(coords_parsed, unit=(u.hr, u.deg))
 
 #SD convert ra and dec to deg
-ra_deg = coords_radec.ra.deg
-dec_deg = coords_radec.dec.deg
+ra_deg = coords.ra.deg
+dec_deg = coords.dec.deg
+
+
+
+#SD finding time of observation
+#SD MST is 7 hrs behind UTC, so add 7 to MST time to get UTC time
+time = Time("2025-2-25 23:00:00") + 7*u.hr
+
+#SD get coordinates of Kitt Peak
+KPNO = EarthLocation.of_site('kpno')
+
+#SD transform ra,dec coordinates to AltAz
+ref_frame = AltAz(location=KPNO, obstime=time)
+coords_altaz = coords.transform_to(ref_frame)
+
+#SD convert alt,az to airmass
+airmasses = coords_altaz.secz
+
+print(airmasses)
+
