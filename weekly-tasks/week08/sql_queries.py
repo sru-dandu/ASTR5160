cdb3bfdcef9862489afcb378803c72f623e6a76e
Srujan Dandu <srujandandu01@gmail.com>
2025-03-10 15:54:35 -0600

    Added comments in Task 1

--- weekly-tasks/week08/sql_queries.py
+++ weekly-tasks/week08/sql_queries.py
@@ -1,20 +1,26 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
 ### TASK 1 (RED) ###
 
+#SD read in csv file
 data = pd.read_csv('result.csv')
 
+#SD extract data columns from csv file
 ra = data['ra']
 dec = data['dec']
 mag_g = data['g']
 
 print('TASK 1:')
+#SD check number of objs in csv file
 print(f'There are {len(ra)} objects in the table, which is about the expected number (350).')
 print('----------')
 
 
+
+### TASK 2 (RED) ###
 
 
