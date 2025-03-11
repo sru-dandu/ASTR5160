1c393134f2da4593d4098b37368e72d97423e30e
Srujan Dandu <srujandandu01@gmail.com>
2025-03-10 20:30:45 -0600

    Used sdssDR9query.py to obtain SDSS optical data for first 100 objs in fits file.

--- weekly-tasks/week08/cross_matching.py
+++ weekly-tasks/week08/cross_matching.py
@@ -1,5 +1,8 @@
 from astropy.table import Table
 import matplotlib.pyplot as plt
+import os
+#importing python file in directory
+import sdssDR9query
 
 
 
@@ -8,8 +11,12 @@ import matplotlib.pyplot as plt
 #SD read in fits file
 objs = Table.read('/d/scratch/ASTR5160/data/first/first_08jul16.fits')
 
+#SD extract ra and dec from file
+ra = objs['RA']
+dec = objs['DEC']
+
 #SD plot ra vs dec
-plt.scatter(objs['RA'], objs['DEC'], s=1)
+plt.scatter(ra, dec, s=1)
 
 #SD labeling the plot
 plt.xlabel('ra [deg]')
@@ -24,3 +31,26 @@ print('----------')
 
 
 
+### TASK 2 (BLACK) ###
+
+print('Task 2:')
+print("Tried out the 'sdssDR9query.py' module.")
+print('----------')
+
+
+
+### TASK 3 (RED) ###
+
+#SD extract first 100 coords
+ra_small = ra[:100]
+dec_small = dec[:100]
+
+print('Task 3:')
+print('Creating file...')
+
+#SD query the coords and append results to file
+for i in range(len(ra_small)):
+	os.system(f"python sdssDR9query.py {ra_small[i]} {dec_small[i]} >> query-results.txt")
+
+print('Created the query results file.')
+print('----------')
