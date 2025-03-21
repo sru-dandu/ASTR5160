0e7073790eba83faa2e60798146959b461a2f797
Srujan Dandu <srujandandu01@gmail.com>
2025-03-21 17:27:51 -0600

    wrote function to find area of lat-lon rectangles. also made argparse more robust by adding possible ranges for lat-lon rectangle bounds

--- homework/hw2.py
+++ homework/hw2.py
@@ -4,8 +4,19 @@ from astropy import units as u
 import argparse
 
 
-
-#def lat_lon_area(ra_min, ra_max, dec_min, dec_max):
+#SD defined function for finding area of lat-lon rectangle
+def lat_lon_area(ra_min, ra_max, dec_min, dec_max):
+	
+	#SD area of lat-lon rectangle, in steradians
+	area = (ra_max - ra_min) * (np.sin(dec_max) - np.sin(dec_min))
+	
+	#SD create plots of lat-lon rectangles
+	
+	
+	return area
+	
+	
+	
 	
 	
 	
@@ -21,15 +32,16 @@ if __name__ == "__main__":
 	
 	#SD inputs for bounds of lat-lon rectangle
 	parser.add_argument("ra_min", type=float,
-			help="['float'] Min ra bound of lat-lon rectangle.")
+			help="['float'] Min ra bound of lat-lon rectangle. Must be between 0 and 2pi rad.")
 	parser.add_argument("ra_max", type=float,
-			help="['float'] Max ra bound of lat-lon rectangle.")
+			help="['float'] Max ra bound of lat-lon rectangle. Must be between 0 and 2pi rad.")
 	parser.add_argument("dec_min", type=float,
-			help="['float'] Min dec bound of lat-lon rectangle.")
+			help="['float'] Min dec bound of lat-lon rectangle. Must be between -pi and pi rad.")
 	parser.add_argument("dec_max", type=float,
-			help="['float'] Max dec bound of lat-lon rectangle.")
+			help="['float'] Max dec bound of lat-lon rectangle. Must be between -pi and pi rad.")
 	#SD let code know if inputs are radians or degrees
-	parser.add_argument("unit", type=str, choices=['radians', 'rad', 'r', 'degrees', 'deg', 'd'],
+	parser.add_argument("unit", type=str,
+			choices=['radians', 'rad', 'r', 'degrees', 'deg', 'd'],
 			help="['str'] Units of inputted values")
 	args = parser.parse_args()
 	
@@ -41,6 +53,19 @@ if __name__ == "__main__":
 	elif args.unit in ['degrees', 'deg', 'd']:
 		ra_min, ra_max, dec_min, dec_max = u.deg.to(u.rad, coords)
 	
+	#SD make sure inputs are in correct ranges
+	#SD ranges: ra from 0 to 2pi, dec from -pi to pi
+	if ra_min < 0 or ra_min > 2*np.pi:
+		raise argparse.ArgumentTypeError(f"ra_min={args.ra_min} {args.unit} outside range of 0 to 2pi radians.")
+	if ra_max < 0 or ra_max > 2*np.pi:
+		raise argparse.ArgumentTypeError(f"ra_max={args.ra_max} {args.unit} outside range of 0 to 2pi radians.")
+	if dec_min < -np.pi or dec_min > np.pi:
+		raise argparse.ArgumentTypeError(f"dec_min={args.dec_min} {args.unit} outside range of -pi to pi radians.")
+	if dec_max < -np.pi or dec_max > np.pi:
+		raise argparse.ArgumentTypeError(f"ra_max={args.dec_max} {args.unit} outside range of -pi to pi radians.")
+	
+	
 	#SD run defined function to find lat-lon rectangle
-	#lat_lon_area(ra_min, ra_max, dec_min, dec_max)
-	print(ra_min, ra_max, dec_min, dec_max)
+	area = lat_lon_area(ra_min, ra_max, dec_min, dec_max)
+	print(area)
+	
