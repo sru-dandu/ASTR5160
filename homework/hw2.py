eede5a6b422985a447e98c880fcbe4b689aa2df3
Srujan Dandu <srujandandu01@gmail.com>
2025-03-21 21:43:19 -0600

    Fixed astropy conversions. Changed output of latlon_area from steradians to deg^2, and removed unit from returned value. Added function to plot 4 lat-lon rectangles in Aitoff projection. Removed redundant conversions and fixed ra nad dec bounds in the if __name__ == '__main__' section.

--- homework/hw2.py
+++ homework/hw2.py
@@ -4,24 +4,73 @@ from astropy import units as u
 import argparse
 
 
+
 #SD defined function for finding area of lat-lon rectangle
-def lat_lon_area(ra_min, ra_max, dec_min, dec_max):
+def latlon_area(ra_min, ra_max, dec_min, dec_max):
 	
-	#SD area of lat-lon rectangle, in steradians
-	area = (ra_max - ra_min) * (np.sin(dec_max) - np.sin(dec_min))
+	#SD make sure inputs are in radians
+	#SD convert each separately in case there is a mix of deg and rad in inputs
+	bounds = [ra_min, ra_max, dec_min, dec_max]
+	ra_min, ra_max, dec_min, dec_max = [b.to(u.rad) for b in bounds]
 	
-	#SD create plots of lat-lon rectangles
+	#SD area of lat-lon rectangle, in square degrees
+	area = (180/np.pi)*(180/np.pi) * (ra_max - ra_min) * (np.sin(dec_max) - np.sin(dec_min))
+	
+	#SD return only value, without the unit
+	return area.value
+
+
+
+#SD make 4 lat-lon rectangles using given ra bounds
+def latlon_plotter(ra_min, ra_max):
+	
+	#SD make sure inputs are in radians
+	#SD need values in radians for plotting in Aitoff
+	ra_min = ra_min.to(u.rad)
+	ra_max = ra_max.to(u.rad)
+	
+	#SD save 4 pairs of dec_mins and dec_maxes to make 4 lat-lon rectangles
+	dec_mins = [-90, -55, 0, 50] * u.deg
+	dec_maxes = [-70, -35, 20, 70] * u.deg
+	
+	#SD convert decs to radians in order to plot in Aitoff
+	dec_mins = dec_mins.to(u.rad)
+	dec_maxes = dec_maxes.to(u.rad)
+	
+	#SD find area of each rectangle
+	areas = [latlon_area(ra_min, ra_max, dec_mins[i], dec_maxes[i]) for i in range(4)]
+	labels = [f"Area = {a:.3f} deg$^2$" for a in areas]
+	
+	#SD create lists for colors for plotting
+	colors = ['blue', 'darkorange', 'green', 'red']
+	
+	#SD strip units from quantities in order to plot
+	#SD because code is throwing error when plotting as quantities
+	ra_min = ra_min.value
+	ra_max = ra_max.value
+	dec_mins = dec_mins.value
+	dec_maxes = dec_maxes.value
 	
 	
-	return area
+	#SD create plot of lat-lon rectangle
+	fig = plt.figure(figsize=(12,10))
+	ax = fig.add_subplot(111, projection="aitoff")
 	
+	#SD plot one side of the rectangle at a time for each (dec_min, dec_max) pair
+	[ax.plot([ra_min, ra_min], [dec_mins[i], dec_maxes[i]], c=colors[i], label=labels[i]) for i in range(4)]
+	[ax.plot([ra_max, ra_max], [dec_mins[i], dec_maxes[i]], c=colors[i]) for i in range(4)]
+	[ax.plot([ra_min, ra_max], [dec_mins[i], dec_mins[i]], c=colors[i]) for i in range(4)]
+	[ax.plot([ra_min, ra_max], [dec_maxes[i], dec_maxes[i]], c=colors[i]) for i in range(4)]
 	
+	ax.grid(color='gray', linestyle='dashed')
+	plt.xlabel('ra [deg]')
+	plt.ylabel('dec [deg]')
+	plt.legend(loc='upper right')
+	plt.show()
 	
-	
-	
-	
-	
-	
+	return
+
+
 
 if __name__ == "__main__":
 	
@@ -32,40 +81,39 @@ if __name__ == "__main__":
 	
 	#SD inputs for bounds of lat-lon rectangle
 	parser.add_argument("ra_min", type=float,
-			help="['float'] Min ra bound of lat-lon rectangle. Must be between 0 and 2pi rad.")
+			help="['float'] Min ra bound of lat-lon rectangle. Must be between -180 and 180 deg.")
 	parser.add_argument("ra_max", type=float,
-			help="['float'] Max ra bound of lat-lon rectangle. Must be between 0 and 2pi rad.")
+			help="['float'] Max ra bound of lat-lon rectangle. Must be between -180 and 180 deg.")
 	parser.add_argument("dec_min", type=float,
-			help="['float'] Min dec bound of lat-lon rectangle. Must be between -pi and pi rad.")
+			help="['float'] Min dec bound of lat-lon rectangle. Must be between -90 and 90 deg.")
 	parser.add_argument("dec_max", type=float,
-			help="['float'] Max dec bound of lat-lon rectangle. Must be between -pi and pi rad.")
+			help="['float'] Max dec bound of lat-lon rectangle. Must be between -90 and 90 deg.")
 	#SD let code know if inputs are radians or degrees
 	parser.add_argument("unit", type=str,
 			choices=['radians', 'rad', 'r', 'degrees', 'deg', 'd'],
-			help="['str'] Units of inputted values")
+			help="['str'] Units of inputted values. All inputs must be in same units.")
 	args = parser.parse_args()
 	
 	
-	#SD convert inputted bounds to radians, if needed
+	#SD add astropy units to inputted values
 	coords = [args.ra_min, args.ra_max, args.dec_min, args.dec_max]
 	if args.unit in ['radians', 'rad', 'r']:
 		ra_min, ra_max, dec_min, dec_max = coords * u.rad
 	elif args.unit in ['degrees', 'deg', 'd']:
-		ra_min, ra_max, dec_min, dec_max = u.deg.to(u.rad, coords)
+		ra_min, ra_max, dec_min, dec_max = coords * u.deg
 	
 	#SD make sure inputs are in correct ranges
-	#SD ranges: ra from 0 to 2pi, dec from -pi to pi
-	if ra_min < 0 or ra_min > 2*np.pi:
-		raise argparse.ArgumentTypeError(f"ra_min={args.ra_min} {args.unit} outside range of 0 to 2pi radians.")
-	if ra_max < 0 or ra_max > 2*np.pi:
-		raise argparse.ArgumentTypeError(f"ra_max={args.ra_max} {args.unit} outside range of 0 to 2pi radians.")
-	if dec_min < -np.pi or dec_min > np.pi:
-		raise argparse.ArgumentTypeError(f"dec_min={args.dec_min} {args.unit} outside range of -pi to pi radians.")
-	if dec_max < -np.pi or dec_max > np.pi:
-		raise argparse.ArgumentTypeError(f"ra_max={args.dec_max} {args.unit} outside range of -pi to pi radians.")
+	#SD Aitoff projection only takes ra from -pi to pi rad, dec from -pi/2 to pi/2 rad
+	if ra_min < -180*u.deg or ra_min > 180*u.deg:
+		raise argparse.ArgumentTypeError(f"ra_min={args.ra_min} {args.unit} outside range of 0 to 360 degrees.")
+	if ra_max < -180*u.deg or ra_max > 180*u.deg:
+		raise argparse.ArgumentTypeError(f"ra_max={args.ra_max} {args.unit} outside range of 0 to 360 degrees.")
+	if dec_min < -90*u.deg or dec_min > 90*u.deg:
+		raise argparse.ArgumentTypeError(f"dec_min={args.dec_min} {args.unit} outside range of -90 to 90 degrees.")
+	if dec_max < -90*u.deg or dec_max > 90*u.deg:
+		raise argparse.ArgumentTypeError(f"ra_max={args.dec_max} {args.unit} outside range of -90 to 90 degrees.")
 	
 	
 	#SD run defined function to find lat-lon rectangle
-	area = lat_lon_area(ra_min, ra_max, dec_min, dec_max)
-	print(area)
+	latlon_plotter(ra_min, ra_max)
 	
