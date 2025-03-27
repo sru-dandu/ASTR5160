5aeb08e25881ff0b641bc53a375038350c6b8fdc
Srujan Dandu <srujandandu01@gmail.com>
2025-03-27 17:30:07 -0600

    extracted coords from all files

--- weekly-tasks/week10/imaging_classification.py
+++ weekly-tasks/week10/imaging_classification.py
@@ -1,5 +1,7 @@
 import numpy as np
 from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy import units as u
 
 
 
@@ -14,7 +16,25 @@ sweepdir = '/d/scratch/ASTR5160/data/legacysurvey/dr9/south/sweep/9.0/'
 sweepfiles = ['sweep-170p025-180p030.fits',
 		'sweep-170p030-180p035.fits',
 		'sweep-180p025-190p030.fits',
-		'sweep-180p030-190p035.fits',]
+		'sweep-180p030-190p035.fits']
 sweepfiles_long = [sweepdir + f for f in sweepfiles]
 sweeptabs = [Table.read(f) for f in sweepfiles_long]
+
+#SD extract coords from given files
+ra = np.append(tab1['RA'], tab2['RA'])
+dec = np.append(tab1['DEC'], tab2['DEC'])
+coords = SkyCoord(ra, dec, unit=u.deg)
+
+#SD extract ra and dec from sweep files
+ra_sweep = np.concatenate((sweepfiles[0]['RA'],
+			sweepfiles[1]['RA'],
+			sweepfiles[2]['RA'],
+			sweepfiles[3]['RA']))
+dec_sweep = np.concatenate((sweepfiles[0]['DEC'],
+			sweepfiles[1]['DEC'],
+			sweepfiles[2]['DEC'],
+			sweepfiles[3]['DEC']))
+coords_sweep = SkyCoord(ra_sweep, dec_sweep)
+
+
 	
