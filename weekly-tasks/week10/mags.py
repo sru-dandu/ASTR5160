7419a926e025f8cbdd380001b959b233336ca459
Srujan Dandu <srujandandu01@gmail.com>
2025-03-26 19:27:51 -0600

    found g and z mags from UBVRI colors, and compared them to g and z mags given in SDSS Navigate Tool

--- weekly-tasks/week10/mags.py
+++ weekly-tasks/week10/mags.py
@@ -13,4 +13,21 @@ U = UminusB + B
 R = V - VminusR
 I = R - RminusI
 
+#SD copying conversion equations from Jester et al. 2005
+#SD https://classic.sdss.org/dr7/algorithms/sdssUBVRITransform.php
+g = V + 0.74*(BminusV) - 0.07
+g_minus_r = 0.93*(BminusV) - 0.06
+r_minus_z = 1.20*(RminusI) - 0.20
+
+#SD calculating z mag
+r = g - g_minus_r
+z = r - r_minus_z
+
+print("TASK 1:")
+print(f"g mag: from conversion = {g} ; from SDSS Navigate Tool = 15.70")
+print(f"z mag: from conversion = {z} ; from SDSS Navigate Tool = 14.55")
+print("The values for g and z obtained by converting from UBVRI match the same values from the SDSS Navigate Tool."
+print('----------')
+
+
 
