67b8b090281eb66ac4d016453a9bf5bb7bcc2fad
Srujan Dandu <srujandandu01@gmail.com>
2025-04-01 16:30:47 -0600

    changed name of function, and added input functionality to the function

--- homework/hw3.py
+++ homework/hw3.py
@@ -3,17 +3,23 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u
 
 
-def main():
+
+def object_extractor(file, coords_center, radius):
+    
     #SD read in FIRST file and extract coords
     table = Table.read('/d/scratch/ASTR5160/data/first/first_08jul16.fits')
     coords = SkyCoord(table['RA'], table['DEC'], unit=u.deg)
     
     #SD find objects within 3 deg of (163 deg, 50 deg)
-    coords_center = SkyCoord(163, 50, unit=u.deg)
-    mask = coords.separation(coords_center) < 3*u.deg
+    mask = coords.separation(coords_center) < radius
     table_survey = table[mask]
     
     return table_survey
 
+
+
 if __name__=='__main__':
-	print(main())
+    
+    filename = '/d/scratch/ASTR5160/data/first/first_08jul16.fits'
+    c_center = SkyCoord(163, 50, unit=u.deg)
+    print(object_extractor(filename, c_center, 3*u.deg))
