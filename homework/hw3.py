3a2b1c6b7aa0b9aceed559a8807c3750b9775a44
Srujan Dandu <srujandandu01@gmail.com>
2025-04-01 19:55:17 -0600

    found the corresponding sweep files for objects in dataset, and read in only those sweep files as astropy tables

--- homework/hw3.py
+++ homework/hw3.py
@@ -2,6 +2,9 @@ from astropy.table import Table
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 
+#SD import a function I previously wrote
+from weekly_tasks.week08.cross_matching import sweep_func
+
 
 
 def object_extractor(file, coords_center, radius):
@@ -14,7 +17,15 @@ def object_extractor(file, coords_center, radius):
     mask = coords.separation(coords_center) < radius
     table_survey = table[mask]
     
-    return table_survey
+    #SD find which sweep files are necessary for cross-matching
+    sweeps_dir = '/d/scratch/ASTR5160/data/legacysurvey/dr9/north/sweep/9.0'
+    sweepfiles = sweep_func(table_survey['RA'], table_survey['DEC'], directory=sweeps_dir)
+    
+    #SD read in sweep files as astropy tables
+    sweepfiles_long = [sweeps_dir + '/' + sf for sf in sweepfiles]
+    table_sweeps = [Table.read(sfl) for sfl in sweepfiles_long]
+    
+    return sweepfiles
 
 
 
@@ -24,3 +35,5 @@ if __name__=='__main__':
     filename = '/d/scratch/ASTR5160/data/first/first_08jul16.fits'
     c_center = SkyCoord(163, 50, unit=u.deg)
     print(object_extractor(filename, c_center, 3*u.deg))
+    
+    
