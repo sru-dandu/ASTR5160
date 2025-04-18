a4993a0732521be4a16eeda31de04d48275e26c7
Srujan Dandu <srujandandu01@gmail.com>
2025-04-18 17:00:56 -0600

    found correlation matrix, and determined which two bins have th highest correlation

--- weekly_tasks/week13/correlated_data.py
+++ weekly_tasks/week13/correlated_data.py
@@ -9,28 +9,38 @@ data = np.loadtxt('/d/scratch/ASTR5160/week13/line.data')
 
 
 #SD find covarianve of dataset
-data_cov = np.cov(data, rowvar=False)
+cov_matrix = np.cov(data, rowvar=False)
 
 print('----------')
 print('TASK 1:')
-print(f"The covariance matrix has the shape {np.shape(data_cov)}.")
+print(f"The covariance matrix has the shape {np.shape(cov_matrix)}.")
 print("This makes sense because there should be one variance value for each bin, resulting in 10 variances values. This makes up the diagonal of the covariance matrix, hence the covariance matrix has to be 10x10.")
 
 #SD extract diagonal of covariance matrix
-cov_diagonal = [data_cov[i][i] for i in range(len(data_cov))]
+cov_diagonal = [cov_matrix[i][i] for i in range(len(cov_matrix))]
 #SD find variance of each bin
 variances = [np.var(data[:,i], ddof=1) for i in range(len(data[0]))]
 
 
 print("Covariance matrix diagonal , Bin variances:")
 print(np.vstack((cov_diagonal, variances)).T)
-print("The covariance matrix's diagonal values matches the variances of the bins."
+print("The covariance matrix's diagonal values matches the variances of the bins.")
 
 
 
-### TASk 2 (RED) ###
+### TASK 2 (RED) ###
 
+std_list = [np.std(data[:,i], ddof=1) for i in range(len(data[0]))]
+std_matrix = np.array([std_list]*10) * np.array([std_list]*10).T
 
+corr_matrix = cov_matrix / std_matrix
 
+corr_unique, unique_idx = np.unique(corr_matrix, return_index=True)
+highest_corr_idx = np.max(unique_idx[corr_unique < 1])
 
+print('TASK 2:')
+print('I found the correlation matrix.')
+print("Ignoring the diagonals, the highest correlation seems to be between bin indices",
+        str(highest_corr_idx)[0], 'and', str(highest_corr_idx)[1])
+print('----------')
 
