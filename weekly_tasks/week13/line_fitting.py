58866a9b9b15a24a04bebd07b7679c9102b0bdf6
Srujan Dandu <srujandandu01@gmail.com>
2025-04-20 13:39:54 -0600

    plotted chi-squared values vs m and b to find best fit m and b values. fixed how I calculated chi squared. added axes labels in previous plot

--- weekly_tasks/week13/line_fitting.py
+++ weekly_tasks/week13/line_fitting.py
@@ -38,8 +38,11 @@ x9 = x0 + 9
 #SD plot datapoints
 x = np.array((x0,x1,x2,x3,x4,x5,x6,x7,x8,x9)).flatten()
 y = data.T.flatten()
-plt.scatter(x, y)
 
+plt.scatter(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
 
 #SD find possible m and b values for the data
 b = np.linspace(4, 6, 10)
@@ -51,9 +54,9 @@ m = np.linspace(m[2], m[7], 10)
 '''
 for i in range(len(m)):
     plt.plot(x, m[i]*x+b[i], label=f'm={m[i]}, b={b[i]}')
+plt.show()
 '''
 
-plt.show()
 
 print('TASK 2:')
 print('Found some possible values for m and b.')
@@ -71,7 +74,7 @@ for mm in m:
     cs = []
     for bb in b:
         ypred = mm*x + bb
-        chi = (np.sum(y - ypred) ** 2) / np.var(y)
+        chi = (np.sum((y - ypred)**2)) / np.var(y)
         cs.append(chi)
     chisquared.append(cs)
 
@@ -82,5 +85,39 @@ chisquared = np.array(chisquared)
 print('TASK 3:')
 print('Founds chi square values for every combination of m and b values I chose in Task 2.')
 print('----------')
+
+
+
+### TASK 4 ###
+
+#SD plot chi squared for each m and each b
+[plt.plot(m, arr, label=f'b = {b[idx]:.2f}') for idx, arr in enumerate(chisquared.T)]
+#plt.plot(
+plt.xlabel('m')
+plt.ylabel(r'$\chi^2$')
+plt.ylim(8,12)
+plt.legend()
+plt.show()
+
+print('TASK 4:')
+print('The minimum chi squared seems to correspond to')
+print('around m = 3 and b = 4.67-4.89.')
+print('----------')
+
+
+
+### TASK 5 ###
+chi_min = np.min(chisquared)
+min_m_idx = np.where(chisquared == chi_min)[0]
+print(m[min_m_idx])
+
+
+
+
+
+
+
+
+
 
 
