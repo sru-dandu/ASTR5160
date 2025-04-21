26b7613ec76b9acc4ce0c06d59ed7e3bf2afc389
Srujan Dandu <srujandandu01@gmail.com>
2025-04-20 18:43:04 -0600

    changed range of m and b values tested from 10 to 100. edited plot of chi square vs m so that only the curve with the lowest chi square is printed. found m and b values at 1 sigma and 2 sigma confidence levels

--- weekly_tasks/week13/line_fitting.py
+++ weekly_tasks/week13/line_fitting.py
@@ -33,12 +33,13 @@ plt.xlabel('x')
 plt.ylabel('bin means')
 plt.show()
 
-#SD find possible m and b values for the data
-b = np.linspace(4, 6, 10)
+#SD find possible b values for the data
+#b = np.linspace(4, 6, 10)
+b = np.linspace(0, 5, 100)
 
-m = np.linspace(2, 4, 10)
-m = np.linspace(m[3], m[6], 10)
-m = np.linspace(m[2], m[7], 10)
+#SD find possible m values for the data
+#m = np.linspace(2, 4, 10)
+m = np.linspace(0, 5, 100)
 
 '''
 for i in range(len(m)):
@@ -69,7 +70,6 @@ for bb in b:
 
 chisquared = np.array(chisquared)
 
-#print(chisquared)
 
 print('TASK 3:')
 print('Founds chi square values for every combination of m and b values I chose in Task 2.')
@@ -80,23 +80,18 @@ print('----------')
 ### TASK 4 (BLACK) ###
 
 #SD plot chi squared for each m and each b
-[plt.plot(m, arr, label=f'b = {b[idx]:.2f}') for idx, arr in enumerate(chisquared)]
-#SD plot where best m is
-chi_min = np.min(chisquared)
-best_m_idx = np.where(chisquared == chi_min)[1][0]
-plt.plot([m[0], m[best_m_idx]], [chi_min, chi_min], c='black', label=f'best m, lowest $\chi^2$')
-plt.plot([m[best_m_idx], m[best_m_idx]], [chi_min-0.5, chi_min], c='black')
+#[plt.plot(m, arr, label=f'b = {b[idx]:.2f}') for idx, arr in enumerate(chisquared)]
+[plt.plot(m, arr, label=f'b = {b[idx]:.3f}') for idx, arr in enumerate(chisquared) if np.min(chisquared) in arr]
 
 plt.xlabel('m')
 plt.ylabel(f'$\chi^2$')
-plt.xlim(m[0], m[-1])
-plt.ylim(chi_min-0.025, chi_min+0.2)   #SD zoom in on relevant region
 plt.legend()
 plt.show()
 
+
 print('TASK 4:')
 print('The minimum chi squared seems to correspond to')
-print('around m = 3 and b = 4.67.')
+print('around m = 3 and b = 4.646.')
 print('----------')
 
 
@@ -104,28 +99,51 @@ print('----------')
 ### TASK 5 (BLACK) ###
 
 #SD find delta chi squared
-delta_chisquared = chisquared - chi_min
+delta_chisquared = chisquared - np.min(chisquared)
 
 #SD find confidence limits
 #SD 10 bins, 2 parameters: ddof = 10-2-1 = 7
-conf_lim = chi2.sf(delta_chisquared, 7)
-conf1sigma = conf_lim > 0.32
-conf2sigma = conf_lim > 0.05
-print(conf1sigma)
-print(conf2sigma)
+conf_lvl = chi2.sf(delta_chisquared, 7)
+conf_lvl_flat = conf_lvl.flatten()
+
+#SD find indices where alpha=0.32 and alpha=0.05
+conf_1sigma_idx = np.argmin(np.abs(conf_lvl_flat - 0.32))
+conf_2sigma_idx = np.argmin(np.abs(conf_lvl_flat - 0.05))
+
+#SD extract the chi squared values and alpha values at those indices
+chi_1sigma = chisquared.flatten()[conf_1sigma_idx]
+chi_2sigma = chisquared.flatten()[conf_2sigma_idx]
+conf_1sigma = conf_lvl_flat[conf_1sigma_idx]
+conf_2sigma = conf_lvl_flat[conf_2sigma_idx]
+
+#SD extract corresponding m and b values
+M, B = np.meshgrid(m, b)
+m_1sigma = M.flatten()[conf_1sigma_idx]
+b_1sigma = B.flatten()[conf_1sigma_idx]
+m_2sigma = M.flatten()[conf_2sigma_idx]
+b_2sigma = B.flatten()[conf_2sigma_idx]
+
+
+print('TASK 5:')
+
+print("The closest we get to a 1 sigma confidence interval in this dataset is " +
+        f"at chisquared={chi_1sigma:.5f}, alpha={conf_1sigma:.5f}.")
+print(f"This corresponds to m={m_1sigma:.3f} and b={b_1sigma:.3f}.")
+
+print("The closest we get to a 2 sigma confidence interval in this dataset is " +
+        f"at chisquared={chi_2sigma:.5f}, alpha={conf_2sigma:.5f}.")
+print(f"This corresponds to m={m_2sigma:.3f} and b={b_2sigma:.3f}.")
 
 
 
 ### TASK 6 (BLACK) ###
-
-plt.figure(figsize=(10,10))
 
 #SD plot datapoints with errorbars
 plt.errorbar(x, means, yerr=np.std(means, ddof=1), elinewidth=1, barsabove=False, fmt='none')
 plt.scatter(x, means, edgecolor='black', label='data')
 
 #SD plot best fit lines and confidence levels
-plt.plot(x, 3*x+4.67, c='red', label='best fit')
+plt.plot(x, 3*x+4.646, c='red', label='best fit')
 
 
 plt.xlabel('x')
