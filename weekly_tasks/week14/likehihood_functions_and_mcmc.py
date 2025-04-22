34897a13c5350205a0fe6e0595ca385c669296ac
Srujan Dandu <srujandandu01@gmail.com>
2025-04-22 00:20:39 -0600

    wrote code to run a Metropolis-Hastings algorithm to create an MCMC chain. Ignore Task 4, it is still in progress

--- weekly_tasks/week14/likehihood_functions_and_mcmc.py
+++ weekly_tasks/week14/likehihood_functions_and_mcmc.py
@@ -61,7 +61,7 @@ def posterior_prob_func(x, ydata, var, m, b, mrange, brange):
     b_low, b_high = np.unique(brange)
     
     #SD set the flat prior according to given acceptable m and b ranges
-    if (m_low < m < m_high) and (b_low < b < b_high):
+    if (m_low <= m <= m_high) and (b_low <= b <= b_high):
         #SD prior = 1, therefore ln(prior) = 0
         ln_prior = 0
     
@@ -75,6 +75,8 @@ def posterior_prob_func(x, ydata, var, m, b, mrange, brange):
     
     
     return ln_posterior_prob, ln_L
+
+
 
 
 
@@ -109,20 +111,99 @@ if __name__ == '__main__':
     ### TASK 2 (RED) ###
 
     print('TASK 2:')
-    print("Created a function to find ln(posterior probability) of a linear dataset.")
+    print("Created a function to find ln(posterior probability) for a linear dataset.")
     print('----------')
     
     
     
     ### TASK 3 (RED) ###
     
+    #SD set initial guesses for parameters
+    m = 3
+    b = 4
+    
+    #SD set acceptable ranges for m and b
+    m_range = [1, 5]
+    b_range = [2, 6]
+    
+    #SD set step size for proposal function
+    #SD (standard deviation of Gaussian centered around m and b)
+    step = 0.02
+    
+    #SD find first iteration of posterior probability and likelihood
+    P, L = posterior_prob_func(x, means, variances, m, b, m_range, b_range)
+    
+    #SD start the MCMC chain with the initial values
+    #SD each item in the chain corresponds to a single proposal (iteration in for loop)
+    chain = [[m, b, L, P]]
+    
+    
+    #SD counters for the for loop
+    iterations = 10000
+    filtered_count = 0
+    
+    for i in range(iterations):
         
-    aaa = posterior_prob_func(x, means, variances, 3, 5, [1,5], [2,6])
+        #SD find new m and b using proposal function
+        m = np.random.normal(m, step)
+        b = np.random.normal(b, step)
+
+        #SD find new posterior probability and likelihood
+        P, L = posterior_prob_func(x, means, variances, m, b, m_range, b_range)
+        
+        #SD find R
+        #SD R = P_new / P_old, or ln(R) = ln(P_new) - ln(P_old)
+        #SD P and P0 here are their natural logs
+        P0 = chain[-1][3]
+        ln_R = P - P0
+        R = np.exp(ln_R)
+        
+        #SD check to see if able to accept new parameters to chain
+        if R >= 1:
+            append_to_chain = True
+        
+        elif R < 1:
+            #SD can only accept new parameters to chain with probability R
+            R_test = np.random.random()
+            if R_test <= R:
+                append_to_chain = True
+            else:
+                append_to_chain = False
+        
+        #SD append new values to MCMC chain
+        if append_to_chain == True:
+            chain.append([m, b, L, P])
+        else:
+            filtered_count += 1
     
-    print(aaa)
+    
+    #SD convert completed chain from list to array
+    chain = np.array(chain)
+    
+    
+    print('TASK 3:')
+    print("Created an MCMC chain using the Metropolis-Hastings algorithm.")
+    print('----------')
     
     
     
+    ### TASK 4 (BLACK) ###
+    
+    #SD total number of proposals accepted to chain is the length of the chain minus 1
+    #SD because initial guess was added to chain first
+    accepted_count = len(chain) - 1
+    
+    print('TASK 4:')
+    print("With step=0.1, I was getting an acceptance rate much less than 30%.")
+    print(f"After changing the step size to {step}, I'm getting an acceptance rate around 30%.")
+    
+    
+    print("total number of proposals:", iterations)
+    print("amount accepted:", accepted_count)
+    print("amount rejected:", filtered_count)
+    print(f"acceptance rate: {100 * accepted_count/iterations}%")
+    
+    #print(chain)
     
     
     
