08d3723992bf9e8e91d48b0ebd8a5166f126a447
Srujan Dandu <srujandandu01@gmail.com>
2025-04-24 13:48:04 -0600

    added test flag in argparse to run code without having to pass a file at the command line every time

--- homework/hw4.py
+++ homework/hw4.py
@@ -138,6 +138,9 @@ if __name__ == '__main__':
     #SD inputs
     parser.add_argument("datafile", type=str,
             help="['str'] Datafile of objects to be scanned for quasars.")
+    parser.add_argument("-t", "--test", action='store_true',
+            help="""Passing this flag runs the code in test mode:
+            Will use a pre-selected sweep file as the input to run the code with.""")
     args = parser.parse_args()
     
     
@@ -145,8 +148,14 @@ if __name__ == '__main__':
     #line_finder()
     
     
-    #SD read in given datafile as an astropy table
-    objs = Table.read(args.datafile)
+    #SD read in datafile as an astropy table
+    if args.test is True:
+        #SD pre-selected sweep file for testing purposes
+        objs = Table.read('/d/scratch/ASTR5160/data/legacysurvey/dr9/south/sweep/9.0/sweep-340p025-350p030.fits')
+        
+    elif args.test is False:
+        #SD user-inputted file
+        objs = Table.read(args.datafile)
     
     #SD run function to find quasars
     quasar_mask = splendid_function(objs)
