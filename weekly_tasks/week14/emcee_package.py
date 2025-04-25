183a77618c1228d800d127fbc6cef36bac3e9103
Srujan Dandu <srujandandu01@gmail.com>
2025-04-25 03:13:07 -0600

    added some comments and moved some code around for clarity/compactness

--- weekly_tasks/week14/emcee_package.py
+++ weekly_tasks/week14/emcee_package.py
@@ -7,23 +7,23 @@ import emcee
 ###SD code copied from Task 1 of previous lecture (likelihood_functions_and_mcmc.py)
 
 #SD import datafile
-#SD unpack=True transposes the array
+#SD unpack=True transposes the array, such that 10 rows of x bins with 20 y datapoints each
 data = np.loadtxt('/d/scratch/ASTR5160/week13/line.data', unpack=True)
 
 #SD create array of x values
 #SD correspond to means of bin ranges (0-1, 1-2, 2-3, ... , 9-10)
 x = np.arange(0.5, 10, 1)
 
-#SD find means of each bin
+#SD find means and variances of each bin
 means = np.mean(data, axis=1)
-
-#SD find variances of each bin
 variances = np.var(data, axis=1, ddof=1)
+
 
 print('TASK 1:')
 print("bin means:", means)
 print("bin variances:", variances)
 print('----------')
+print(np.shape(data))
 
 
 
