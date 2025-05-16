96082c58c2a284c3d97ed3665524d83ec24a5708
Srujan Dandu <srujandandu01@gmail.com>
2025-05-15 21:11:59 -0600

    fixed priors for linear fit. removed unnecessary code. moved linear fit code to its own function. copied in code to do quadratic fit using emcee

--- homework/final_project.py
+++ homework/final_project.py
@@ -33,7 +33,7 @@ def post_prob_func_linear(params, x, ydata, var):
     m, b = params
     
     #SD set the flat prior according to given acceptable m and b ranges
-    if (0 <= m <= -5) and (0 <= b <= 5):
+    if (-5 <= m <= 0) and (0 <= b <= 5):
         #SD prior = 1, therefore ln(prior) = 0
         ln_prior = 0
     else:
@@ -111,7 +111,7 @@ def post_prob_func_quadratic(params, x, ydata, var):
     a2, a1, a0 = params
     
     #SD set the flat prior according to given acceptable m and b ranges
-    if (1 <= a2 <= 5) and (1 <= a1 <= 5) and (1 <= a0 <= 5):
+    if (1 <= a2 <= 5) and (-5 <= a1 <= 0) and (1 <= a0 <= 10):
         #SD prior = 1, therefore ln(prior) = 0
         ln_prior = 0
     else:
@@ -125,6 +125,128 @@ def post_prob_func_quadratic(params, x, ydata, var):
 
 
 
+###SD code adapted from https://emcee.readthedocs.io/en/stable/tutorials/line/
+def finalproject_linearfit(x, y, yerr):
+    """Contains code for finding a linear best-fit line to the data given for the final project.
+    
+    INPUTS
+    ------
+    None
+    
+    RETURNS
+    -------
+    None
+    
+    """
+    
+    #SD initial guesses for m and b
+    initial = np.array([-1, 4])
+
+
+    #SD marginalization and uncertainty estimation
+    #SD use emcee package to run mcmc algorithm
+    pos = initial + 1e-4 * np.random.randn(32, 2)
+    nwalkers, ndim = pos.shape
+    sampler = emcee.EnsembleSampler(
+        nwalkers, ndim, post_prob_func_linear, args=(x, y, yerr**2)
+        )
+    sampler.run_mcmc(pos, 5000, progress=True)
+
+
+    #SD discard some of the initial steps and flatten the chain
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    print(flat_samples.shape)
+
+
+    #SD corner plot
+    labels=['m', 'b']
+    corner.corner(flat_samples, labels=labels)
+    plt.show()
+
+
+    #SD print the best fit m and b values with errors
+    bestfit_params = []
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        bestfit_params.append(mcmc[1])
+        print(f"{labels[i]} = {mcmc[1]} ; errors: + {q[0]} , - {q[1]}")
+
+
+
+    #SD plot datapoints with the best-fit line from
+    plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0, label='data')
+    plt.plot(x, bestfit_params[0]*x+bestfit_params[1], label='best fit line')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+    return
+
+
+
+###SD code adapted from https://emcee.readthedocs.io/en/stable/tutorials/line/
+def finalproject_quadfit(x, y, yerr):
+    """Contains code for finding a linear best-fit line to the data given for the final project.
+    
+    INPUTS
+    ------
+    None
+    
+    RETURNS
+    -------
+    None
+    
+    """
+    
+    #SD initial guesses for a2, a1, a0
+    initial = np.array([3, -1, 5])
+
+
+    #SD marginalization and uncertainty estimation
+    #SD use emcee package to run mcmc algorithm
+    pos = initial + 1e-4 * np.random.randn(32, 3)
+    nwalkers, ndim = pos.shape
+    sampler = emcee.EnsembleSampler(
+        nwalkers, ndim, post_prob_func_quadratic, args=(x, y, yerr**2)
+        )
+    sampler.run_mcmc(pos, 5000, progress=True)
+
+
+    #SD discard some of the initial steps and flatten the chain
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    print(flat_samples.shape)
+
+
+    #SD corner plot
+    labels=['a2', 'a1', 'a0']
+    corner.corner(flat_samples, labels=labels)
+    plt.show()
+
+
+    #SD print the best fit a2, a1, a0 values with errors
+    bestfit_params = []
+    for i in range(ndim):
+        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+        q = np.diff(mcmc)
+        bestfit_params.append(mcmc[1])
+        print(f"{labels[i]} = {mcmc[1]} ; errors: + {q[0]} , - {q[1]}")
+
+
+
+    #SD plot datapoints with the best-fit line from
+    plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0, label='data')
+    plt.plot(x, bestfit_params[0]*(x**2)+bestfit_params[1]*x+bestfit_params[0], label='best fit line')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+    return
+
+
+
 
 
 if __name__ == '__main__':
@@ -134,85 +256,14 @@ if __name__ == '__main__':
     x = data['x']
     y = data['y']
     yerr = data['yerr']
-    variances = yerr**2
+    #variances = yerr**2
+
     
+    finalproject_linearfit(x, y, yerr)
     
-    ###SD code adapted from https://emcee.readthedocs.io/en/stable/tutorials/line/
-    
-    ###SD maximum likelihood estimation
-    #SD initial guesses for m and b
-    initial = np.array([-1, 3])
-    #SD maximize the likelihood function to find best m and b
-    neg_likelihood_func = lambda *args: -1*likelihood_func_linear(*args)
-    soln = minimize(neg_likelihood_func, initial, args=(x, y, variances))
-    m_ml, b_ml = soln.x
-
-    print("Maximum likelihood estimates:")
-    print(f"m = {m_ml:.3f}")
-    print(f"b = {b_ml:.3f}")
+    finalproject_quadfit(x, y, yerr)
 
 
 
-    #SD marginalization and uncertainty estimation
-    #SD use emcee package to run mcmc algorithm
-    pos = soln.x + 1e-4 * np.random.randn(32, 2)
-    nwalkers, ndim = pos.shape
-    sampler = emcee.EnsembleSampler(
-        nwalkers, ndim, post_prob_func_linear, args=(x, y, variances)
-        )
-    sampler.run_mcmc(pos, 5000, progress=True)
 
 
-    #SD plotting time series of mcmc chain
-    fig, axes = plt.subplots(2, figsize=(10, 7), sharex=True)
-    samples = sampler.get_chain()
-    labels = ["m", "b"]
-    for i in range(ndim):
-        ax = axes[i]
-        ax.plot(samples[:, :, i], "k", alpha=0.3)
-        ax.set_xlim(0, len(samples))
-        ax.set_ylabel(labels[i])
-        ax.yaxis.set_label_coords(-0.1, 0.5)
-
-    axes[-1].set_xlabel("step number")
-
-    plt.show()
-    
-    
-    #SD discard some of the initial steps and flatten the chain
-    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
-    print(flat_samples.shape)
-
-
-    #SD corner plot
-    corner.corner(flat_samples, labels=labels)
-    plt.show()
-
-
-    #SD plot datapoints with some of the walks from the chain
-    inds = np.random.randint(len(flat_samples), size=100)
-    for ind in inds:
-        sample = flat_samples[ind]
-        if ind==inds[0]:
-            plt.plot(x, np.dot(np.vander(x, 2), sample[:2]), "C1", alpha=0.1, label='walk samples')
-        else:    
-            plt.plot(x, np.dot(np.vander(x, 2), sample[:2]), "C1", alpha=0.1)
-    plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0, label='data')
-    plt.plot(x, m_ml*x+b_ml, label='best fit')
-    plt.legend(fontsize=14)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.show()
-
-
-    #SD print the best fit m and b values with errors
-    for i in range(ndim):
-        mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
-        q = np.diff(mcmc)
-        print(f"{labels[i]} = {mcmc[1]} ; errors: + {q[0]} , - {q[1]}")
-
-
-    print('----------')
-    
-    
-    
